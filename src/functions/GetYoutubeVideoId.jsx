fe19538ed7531942e6920ccd2c74b7afc92d01ed
Prince <princeekpinse97@gmail.com>
2025-01-12 16:34:10 +0100

    Resolve GetId fonction

--- src/functions/GetYoutubeVideoId.jsx
+++ src/functions/GetYoutubeVideoId.jsx
@@ -1,10 +1,11 @@
-
+// Fonction pour extraire l'ID des vidéos YouTube à partir d'URL au format varié
 export function getYouTubeVideoId(url) {
-    const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n]+(?:\/|(?:v|e(?:mbed)?)\/|[?&]v=?)([^"&?\/ ]{11})|(?:v|e(?:mbed)?)\/([^"&?\/ ]{11}))|youtu\.be\/([^"&?\/ ]{11}))/;
-    const match = url.match(regex);
-    return match ? match[1] || match[2] || match[3] : null;
-  }
-  
-  // // Exemples d'utilisation
-  // console.log(getYouTubeVideoId('https://www.youtube.com/watch?v=23Qw4w9WgXcQQw4w'));
-  // console.log(getYouTubeVideoId('https://youtu.be/dQw4w9WgXcQ'));
+  const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^/\n]+(?:\/|(?:v|e(?:mbed)?)\/|[?&]v=)?([^"&?/ ]{11})|(?:v|e(?:mbed)?)\/([^"&?/ ]{11}))|youtu\.be\/([^"&?/ ]{11}))/;
+  const match = url.match(regex);
+  return match ? match[1] || match[2] || match[3] : null;
+}
+
+// Exemples d'utilisation
+console.log(getYouTubeVideoId('https://www.youtube.com/watch?v=dQw4w9WgXcQ'));
+console.log(getYouTubeVideoId('https://youtu.be/dQw4w9WgXcQ'));
+console.log(getYouTubeVideoId('https://www.youtube.com/embed/dQw4w9WgXcQ'));
